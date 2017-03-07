56e60077cb19221c9d0978b52e5327bac80f46a4
Kent Fredric <kentfredric@gmail.com>
2017-03-08 02:19:18 +1300

    Build results of a9c406f (on master)

--- lib/Cave/Wrapper.pm
+++ lib/Cave/Wrapper.pm
@@ -6,7 +6,7 @@ package Cave::Wrapper;
 
 # ABSTRACT: A Wrapper to the Paludis 'cave' Client.
 
-our $VERSION = '1.000001';
+our $VERSION = '1.000002';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -166,7 +166,7 @@ Cave::Wrapper - A Wrapper to the Paludis 'cave' Client.
 
 =head1 VERSION
 
-version 1.000001
+version 1.000002
 
 =head1 DESCRIPTION
 
